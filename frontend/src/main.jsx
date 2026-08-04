ff4798aecdd21aac6aa23871847d4834e8a00330
Abhiraj-creator <abhirajbhati676@gmail.com>
2026-08-04 10:02:28 +0530

    fix: remove StrictMode wrapper from main component rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './features/shared/styles/golbal.scss'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <App />
-  </StrictMode>,
 )
